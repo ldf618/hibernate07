fbb4d3ce5be93a16a7f490832410b1c74578c0a2
ldf618 <85827733+ldf618@users.noreply.github.com>
2022-02-06 12:33:24 +0100

    Add files via upload

---
+++ hibernate07/src/test/java/util/model/Thing.java
@@ -0,0 +1,16 @@
+package util.model;
+
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity(name = "Thing")
+@Data
+public class Thing {
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  Integer id;
+  @Column
+  String name;
+}
